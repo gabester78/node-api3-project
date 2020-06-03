505d2f56a01cfa1d5f7b534d14c9c3ed514593aa
Gabriel Romero <gabester78@gmail.com>
2020-06-03 18:39:42 -0500

    User endpoints done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+const server = require("./server");
+
+const port = 5000;
+
+server.listen(port, () =>
+  console.log(`\n*** API running on port ${port} ***\n`)
+);
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,91 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
+// const server = express();
+const db = require("./userDb");
+const postDb = require("../posts/postDb");
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post("/", (req, res) => {
+  db.insert(req.body)
+    .then((article) => {
+      res.status(201).json(article);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "There was an error while saving the post to the database",
+      });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", (req, res) => {
+  postDb
+    .insert(req.body)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "can't get the posts" });
+    });
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+  db.get()
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Users not found" });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", (req, res) => {
+  let id = req.params.id;
+  db.getById(id)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "cannot get the user" });
+    });
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get("/:id/posts", (req, res) => {
+  let id = req.params.id;
+  db.getUserPosts(id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "can't get the posts" });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", (req, res) => {
+  let id = req.params.id;
+  db.remove(id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "can't remove user" });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:id", (req, res) => {
+  let id = req.params.id;
+  db.update(id, req.body)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "can't update user" });
+    });
 });
 
 //custom middleware
