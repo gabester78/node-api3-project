20efb9f63bb4cfbe90756459cc87de29980a0957
Gabriel Romero <gabester78@gmail.com>
2020-06-04 16:17:17 -0500

    Ready to be deployed.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./server");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () =>
   console.log(`\n*** API running on port ${port} ***\n`)
